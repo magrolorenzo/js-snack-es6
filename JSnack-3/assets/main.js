f5026e267a4da24ec6508faf8f986cb485f74a6e
Lorenzo Magro <magrolorenzo@gmail.com>
2020-11-04 17:20:32 +0100

    Fatto ma non so se il metodo per non modificare il primo array sia giusto

--- JSnack-3/assets/main.js
+++ JSnack-3/assets/main.js
@@ -23,7 +23,26 @@ const main_array = [
     },
 ];
 
+let min_ascii = 97;
+let max_ascii = 122;
+
 const new_array = main_array.map((element) =>{
-    return element;
+    let {name, surname} = element;
+    let new_elem = {};
+    new_elem.name = name;
+    new_elem.surname = surname;
+    return new_elem;
 });
+
+new_array.forEach((element) => element.position = String.fromCharCode(getRndInteger(min_ascii,max_ascii)));
+
+console.log(main_array);
 console.log(new_array);
+
+
+
+
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
